90484495e28fd62ed3b706da13a525afe9ef6739
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-10-27 04:16:44 +0200

    Use environment variable for database

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const cors         = require('cors');
 
 
 mongoose
-  .connect('mongodb://localhost/pizza-shop-server', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
